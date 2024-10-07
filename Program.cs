8ff2ce3f734f0a16d369d0032da4a573c5c694a8
Nash-Finkbeiner-Student <nafi36a@student.oaisd.org>
2024-10-07 13:52:14 +0000

    Made And Fixed Code

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("My name is Nash Finkbeiner");
+
+            int age = 18;
+            string firstname = "Nash";
+            string lastname = "Finkbeiner";
+            string greeting = "Hello, World!";
+            double money = 5.68;
+
+            Console.WriteLine(greeting + " My name is " + firstname + " " + lastname + ". I am " + age + " years old " + "and I spent $" + money + " on a Root Beer.");
+        }
+    }
